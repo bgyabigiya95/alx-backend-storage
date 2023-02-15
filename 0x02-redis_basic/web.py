4e2e2afcb898702cf5c9a80a272a91dbc9ba71d0
root <bgyabigiya95@gmail.com>
2023-02-15 20:18:20 +0000

    commit

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
-"""
-In this tasks, we will implement a get_page function
-(prototype: def get_page(url: str) -> str:). The core of the function
-is very simple. It uses the requests module to obtain the HTML
-content of a particular URL and returns it
-"""
-import requests
+'''A module with tools for request caching and tracking.
+'''
 import redis
+import requests
+from functools import wraps
+from typing import Callable
 
 
-r = redis.Redis()
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
 def get_page(url: str) -> str:
-    """
-    Inside get_page track how many times a particular URL was
-    accessed in the key count:{url} and cache the result with
-    an expiration time of 10 seconds
-    """
-    html = r.get(url)
-    if html:
-        return html.decode('utf-8')
-    else:
-        response = requests.get(url)
-        html = response.text
-        r.set(url, html, ex=10)
-        return html
-
-
-if __name__ == "__main__":
-    get_page("http://slowwly.robertomurray.co.uk")
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
