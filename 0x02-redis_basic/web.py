19b3db2a54b49681f254a872da87469766d4481a
root <bgyabigiya95@gmail.com>
2023-02-15 20:27:52 +0000

    commit

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+"""
+Implementing an expiring web cache and tracker.
+"""
+
 import redis
 import requests
 from functools import wraps
@@ -8,17 +10,22 @@ from typing import Callable
 
 
 redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+"""
+The module-level Redis instance.
+"""
 
 
 def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+    """
+    Caches the output of fetched data.
+    """
+
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
+    def wrapper(url) -> str:
+        """
+        The wrapper function for caching the output.
+        """
+
         redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
@@ -27,12 +34,14 @@ def data_cacher(method: Callable) -> Callable:
         redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
         return result
-    return invoker
+    return wrapper
 
 
 @data_cacher
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
+    """
+    Returns the content of a URL after caching the
+    request's response, and tracking the request.
+    """
+
     return requests.get(url).text
